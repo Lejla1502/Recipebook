829a89e3d6d34b0a35e5b863cc0ee18603a8a177
Lejla1502 <lejlasphotmail.com>
2021-12-07 12:36:33 +0100

    Implementing navbar functionality

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RecipesComponent } from './recipes/recipes.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'recipes-component', component: RecipesComponent },
+  { path: 'shopping-list-component', component: ShoppingListComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
