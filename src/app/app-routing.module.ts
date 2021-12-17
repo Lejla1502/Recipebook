f734ffe9b7843dc9dabb9cf4d6e850263a4921c4
Lejla1502 <lejlasphotmail.com>
2021-12-17 16:41:03 +0100

    Setting up the routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,8 +4,9 @@ import { RecipesComponent } from './recipes/recipes.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 
 const routes: Routes = [
-  { path: 'recipes-component', component: RecipesComponent },
-  { path: 'shopping-list-component', component: ShoppingListComponent },
+  {path:'', redirectTo:'/recipes', pathMatch:'full'},
+  { path: 'recipes', component: RecipesComponent},
+  { path: 'shopping-list', component: ShoppingListComponent },
 ];
 
 @NgModule({
