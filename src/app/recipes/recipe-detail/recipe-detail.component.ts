66637398d6ed4aec74b753c37d20cf8e145bae8a
Lejla1502 <lejlasp@hotmail.com>
2021-12-19 15:13:05 +0100

    Setting up router link active on recipe item clicked

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -22,9 +22,9 @@ export class RecipeDetailComponent implements OnInit {
    
     this.route.params.subscribe(   //different way to initialize id
       (params:Params)=>{
-        this.id=+params['id'];
+       // this.id=+params['id'];
       
-        this.recipe=this.recipeService.getRecipe(this.id);
+        this.recipe=this.recipeService.getRecipe(+params['id']);
       }
         );
 
