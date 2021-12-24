0d2123d7117fd09ab8fdb940aff41fb88908c8d8
Lejla1502 <lejlasphotmail.com>
2021-12-24 11:37:35 +0100

    Providing the recipe service correctly

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,6 +16,7 @@ import { ShoppingListService } from './shopping-list/shopping-list.service';
 import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
 import { RecipeResolver } from './recipes/recipe-resolver.service';
 import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
+import { RecipeService } from './recipes/recipe.service';
 
 @NgModule({
   declarations: [
@@ -37,7 +38,7 @@ import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component
     FormsModule,
     ReactiveFormsModule
   ],
-  providers: [ShoppingListService, RecipeResolver],
+  providers: [ShoppingListService, RecipeResolver, RecipeService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
