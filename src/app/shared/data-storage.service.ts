3ad79dfcb32e6dfd8187e2eb568956a7c7208937
Lejla1502 <lejlasphotmail.com>
2022-02-07 16:45:08 +0100

    Saving recipes onto firebase

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,8 +1,18 @@
+import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { RecipeService } from "../recipes/recipe.service";
 
 @Injectable({providedIn: 'root'})
 export class DataStorageService {
-    constructor(){
+    constructor(private http:HttpClient, private recipeService:RecipeService){
         
+    }
+
+    storeRecipes(){
+        const recipes=this.recipeService.getRecipes();
+        //put request works in such a way that any 
+        this.http.put('https://ng-course-recipe-book-8865c-default-rtdb.firebaseio.com/recipes.json', recipes).subscribe(responseData=>{
+            console.log(responseData);
+        });
     }
 }
