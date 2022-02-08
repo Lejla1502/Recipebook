1936e61157e07f6fed5b4431eeb3480a16909af9
Lejla1502 <lejlasphotmail.com>
2022-02-08 09:26:57 +0100

    Transforming response data

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -12,32 +12,34 @@ export class RecipeService{
 
 
     
-    private recipes:Recipe[]=[
-        new Recipe("Tasty Schnitzel", "this is simply a test",
-        "https://upload.wikimedia.org/wikipedia/commons/5/57/990402-ians-recipe-01-IMG_4724.jpg", 
-        [
-            new Ingredient('Meat',1),
-            new Ingredient('French Fries', 20)
-        ]),
-        new Recipe("Big Fat Burger", "this is simply a test 2",
-        "https://get.pxhere.com/photo/food-dish-cuisine-ingredient-produce-recipe-staple-food-side-dish-stuffing-vegetable-meat-pilaf-Southwestern-united-states-food-jambalaya-Pebre-salad-1613433.jpg",
-        [
-            new Ingredient('Buns', 2),
-            new Ingredient('Meat', 1)
-        ]),
-        new Recipe("Pasta Italiana", "this is simply a test 2",
-        "https://get.pxhere.com/photo/food-dish-cuisine-ingredient-produce-recipe-staple-food-side-dish-stuffing-vegetable-meat-pilaf-Southwestern-united-states-food-jambalaya-Pebre-salad-1613433.jpg",
-        [
-            new Ingredient('Pasta', 2),
-            new Ingredient('Sos', 1)
-        ]),
-        new Recipe("Pizza", "this is simply a test 2",
-        "https://get.pxhere.com/photo/food-dish-cuisine-ingredient-produce-recipe-staple-food-side-dish-stuffing-vegetable-meat-pilaf-Southwestern-united-states-food-jambalaya-Pebre-salad-1613433.jpg",
-        [
-            new Ingredient('Dough', 1),
-            new Ingredient('Salamy', 1)
-        ])
-      ];
+    // private recipes:Recipe[]=[
+    //     new Recipe("Tasty Schnitzel", "this is simply a test",
+    //     "https://upload.wikimedia.org/wikipedia/commons/5/57/990402-ians-recipe-01-IMG_4724.jpg", 
+    //     [
+    //         new Ingredient('Meat',1),
+    //         new Ingredient('French Fries', 20)
+    //     ]),
+    //     new Recipe("Big Fat Burger", "this is simply a test 2",
+    //     "https://get.pxhere.com/photo/food-dish-cuisine-ingredient-produce-recipe-staple-food-side-dish-stuffing-vegetable-meat-pilaf-Southwestern-united-states-food-jambalaya-Pebre-salad-1613433.jpg",
+    //     [
+    //         new Ingredient('Buns', 2),
+    //         new Ingredient('Meat', 1)
+    //     ]),
+    //     new Recipe("Pasta Italiana", "this is simply a test 2",
+    //     "https://get.pxhere.com/photo/food-dish-cuisine-ingredient-produce-recipe-staple-food-side-dish-stuffing-vegetable-meat-pilaf-Southwestern-united-states-food-jambalaya-Pebre-salad-1613433.jpg",
+    //     [
+    //         new Ingredient('Pasta', 2),
+    //         new Ingredient('Sos', 1)
+    //     ]),
+    //     new Recipe("Pizza", "this is simply a test 2",
+    //     "https://get.pxhere.com/photo/food-dish-cuisine-ingredient-produce-recipe-staple-food-side-dish-stuffing-vegetable-meat-pilaf-Southwestern-united-states-food-jambalaya-Pebre-salad-1613433.jpg",
+    //     [
+    //         new Ingredient('Dough', 1),
+    //         new Ingredient('Salamy', 1)
+    //     ])
+    //   ];
+
+    private recipes:Recipe[]=[];
 
       constructor(){}
 
