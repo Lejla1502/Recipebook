6b1726365fb945dd208406e4b0a04f0adbc55392
Lejla1502 <lejlasphotmail.com>
2022-02-11 10:01:32 +0100

    Adding token to outgoing request pt2

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,6 +1,6 @@
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpParams } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import { map, tap } from "rxjs/operators";
+import { exhaustMap, map, take, tap } from "rxjs/operators";
 import { AuthService } from "../auth/auth/auth.service";
 import { Recipe } from "../recipes/recipe.model";
 import { RecipeService } from "../recipes/recipe.service";
@@ -22,15 +22,30 @@ export class DataStorageService {
 
     fetchRecipes()
     {
-        return this.http.get<Recipe[]>('https://ng-course-recipe-book-8865c-default-rtdb.firebaseio.com/recipes.json')
-        .pipe(map(recipes=>{
+        //taking one value from the observable and automatically unsubscribing - take(1)
+        //plus we need to pipe user obervable and http observable together into one big observable
+        //we do this with exhaustMap: it waits for the user observable to complete which will happen after we took the latest user
+        //after we get the user, we pass it in a function and then we return a new observable which will replace our previous observable in the 
+        //entire observable chain
+        //in the end the entire observable chain switches to http observable which returns recipes
+
+        return this.authService.user.pipe(take(1), exhaustMap(user=>{
+            return this.http.get<Recipe[]>('https://ng-course-recipe-book-8865c-default-rtdb.firebaseio.com/recipes.json',
+            {
+                params: new HttpParams().set('auth', user.token)
+            });
+
+        }),
+        map(recipes=>{
             return recipes.map(recipe=>{
                 return {...recipe, ingredients:recipe.ingredients?recipe.ingredients:[]};
             });
-        }), tap(recipes=>{
+        }), 
+        tap(recipes=>{
                 this.recipeService.setRecipes(recipes);
         })
         );
+
         
     }
 } 
