5c6e5789953ade6ac0a285be764abb39c3b8740e
Lejla1502 <lejlasphotmail.com>
2022-02-15 15:57:25 +0100

    Shared module pt1

--- src/app/shopping-list/shopping-list.module.ts
+++ src/app/shopping-list/shopping-list.module.ts
@@ -1,7 +1,7 @@
-import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
-import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { FormsModule} from "@angular/forms";
 import { RouterModule } from "@angular/router";
+import { SharedModule } from "../shared/shared.module";
 import { ShoppingEditComponent } from "./shopping-edit/shopping-edit.component";
 import { ShoppingListComponent } from "./shopping-list.component";
 
@@ -11,10 +11,13 @@ import { ShoppingListComponent } from "./shopping-list.component";
         ShoppingEditComponent,
     ],
     imports: [
-        RouterModule, CommonModule, ReactiveFormsModule, FormsModule,  
-        [RouterModule.forChild(  [{ path: 'shopping-list', component: ShoppingListComponent }]  )],
+        FormsModule, 
+        RouterModule.forChild([
+            { path: 'shopping-list', component: ShoppingListComponent },
+         ]),
+        SharedModule
     ]
-    
+   
 })
 
 export class ShoppingListModule{
