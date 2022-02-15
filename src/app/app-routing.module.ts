3223b2e4e4b45ae02ee7379535af5050948f265a
Lejla1502 <lejlasphotmail.com>
2022-02-15 12:47:03 +0100

    Adding routes to feature modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,6 @@ import { AuthComponent } from './auth/auth/auth.component';
 import { AuthGuard } from './auth/auth/auth.guard';
 import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
 import { LoginComponent } from './login/login.component';
-import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
-import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
-import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
-import { RecipesResolverService } from './recipes/recipes-resolver.service';
-import { RecipesComponent } from './recipes/recipes.component';
 import { RegisterComponent } from './register/register.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 
@@ -18,15 +13,6 @@ const routes: Routes = [
   {path:'login', component: LoginComponent},
   {path:'forgot-password', component:ForgotPasswordComponent},
   {path:'register', component:RegisterComponent},
-  { path: 'recipes', component: RecipesComponent, 
-  canActivate: [AuthGuard],
-  children:[
-    {path : '', component:RecipeStartComponent},
-    {path:'new-recipe', component:RecipeEditComponent}, //this path MUST go before all paths with id, so it can actually work
-    {path:':id', component:RecipeDetailComponent, resolve:[RecipesResolverService]},
-    {path:':id/edit', component:RecipeEditComponent, resolve:[RecipesResolverService]}
-
-  ]},
   { path: 'shopping-list', component: ShoppingListComponent },
 ];
 
--- src/app/recipes/recipes.module.ts
+++ src/app/recipes/recipes.module.ts
@@ -7,6 +7,7 @@ import { RecipeEditComponent } from "./recipe-edit/recipe-edit.component";
 import { RecipeItemComponent } from "./recipe-list/recipe-item/recipe-item.component";
 import { RecipeListComponent } from "./recipe-list/recipe-list.component";
 import { RecipeStartComponent } from "./recipe-start/recipe-start.component";
+import { RecipesRoutingModule } from "./recipes-routing.module";
 import { RecipesComponent } from "./recipes.component";
 
 //NgModule decorator takes an object which we pass in to configure the module
@@ -23,7 +24,7 @@ import { RecipesComponent } from "./recipes.component";
         RecipeEditComponent
     ],
     imports: [
-        RouterModule, CommonModule, ReactiveFormsModule
+        RouterModule, CommonModule, ReactiveFormsModule, RecipesRoutingModule
     ],
     exports: [
         RecipesComponent,
