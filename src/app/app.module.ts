9348c4cd67fe6a64784c23a13cac455fb660387e
Lejla1502 <lejlasphotmail.com>
2022-02-16 09:06:11 +0100

    Adding core module with services from app mpdule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {HttpClientModule, HTTP_INTERCEPTORS} from '@angular/common/http';
+import {HttpClientModule} from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { ShoppingListService } from './shopping-list/shopping-list.service';
-import { RecipeResolver } from './recipes/recipe-resolver.service';
-import { RecipeService } from './recipes/recipe.service';
 import { LoginComponent } from './login/login.component';
-import { UserService } from './user.service';
 import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
 import { RegisterComponent } from './register/register.component';
 import { AuthComponent } from './auth/auth/auth.component';
-import { AuthService } from './auth/auth/auth.service';
-import { AuthInterceptorService } from './auth/auth-interceptor.service';
 import { RecipesModule } from './recipes/recipes.module';
 import { ShoppingListModule } from './shopping-list/shopping-list.module';
 import { SharedModule } from './shared/shared.module';
+import { CoreModule } from './core.module';
 
 @NgModule({
   declarations: [
@@ -39,10 +34,9 @@ import { SharedModule } from './shared/shared.module';
     HttpClientModule,
     RecipesModule,
     ShoppingListModule,
-    SharedModule
+    SharedModule,
+    CoreModule 
   ],
-  providers: [ShoppingListService, RecipeResolver, RecipeService, UserService, AuthService, 
-    {provide: HTTP_INTERCEPTORS, useClass: AuthInterceptorService, multi:true }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
